d1b187e8095893d89fdcd7c011de3730c39e877e
computermouth <computermouth@crunchbangplusplus.org>
2016-03-06 02:50:47 -0800

    mooooaaaar shaaaaapes

--- src/main.rs
+++ src/main.rs
@@ -6,65 +6,47 @@ use sdl2::keyboard::Keycode;
 
 mod modrect;
 mod shapes;
-//mod game_state;
-//mod action;
+mod game_state;
+mod logo;
+mod logo_shapes;
+
+use game_state::GameState;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("SLAPFIGHT", 600, 440)
+    let window = video_subsystem.window("SLAPFIGHT", 640, 440)
         .position_centered()
         .build()
         .unwrap();
 
-    let mut renderer = window.renderer().build().unwrap();
+    let mut renderer = window.renderer()
+		.present_vsync()
+		.build()
+		.unwrap();
+    
+    let mut state = GameState::Logo;
+    
 	'running: loop {
 		
-		renderer.set_draw_color(Color::RGBA(0, 0, 0, 255));
+		renderer.set_draw_color(Color::RGBA(30, 30, 39, 255));
 		renderer.clear();
 		
-		let purp = modrect::MyRect::new()
-			.x(120)
-			.y(120)
-			.w(120)
-			.h(120)
-			.r(200)
-			.g(0)
-			.b(200)
-			.a(120)
-			.finalize();
-			
-
-		renderer = modrect::draw(renderer, purp);
+		let mut event_pump = sdl_context.event_pump().unwrap();
 		
-		let green = modrect::MyRect::new()
-			.x(200)
-			.y(200)
-			.w(120)
-			.h(120)
-			.r(0)
-			.g(200)
-			.b(0)
-			.a(120)
-			.finalize();
-			
-		//~ renderer.set_draw_color(green.colorout());
-		//~ renderer.fill_rect(green.rectout()).unwrap();
-
-		renderer = modrect::draw(renderer, green);
-		renderer = modrect::draw(renderer, shapes::RED);
+		let mut some: u8 = 12;
 		
-		//~ let green = modrect::MyRect::new(100, 100, 100, 100, 0, 150, 0, 255);
-		//~ renderer.set_draw_color(green.colorout());
-		//~ renderer.fill_rect(green.rectout()).unwrap();
-		//~ green.draw();
+		match state {
+			GameState::Logo		=> renderer = logo::logo_scene(renderer),
+			GameState::Intro	=> println!("intro"),
+			GameState::Play		=> println!("play"),
+			GameState::Over		=> println!("over"),
+		}
 		
 		renderer.present();
 
-		let mut event_pump = sdl_context.event_pump().unwrap();
+		
 
-    
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -73,6 +55,5 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
     }
 }
