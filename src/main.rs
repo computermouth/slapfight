4b4f86b32dc55ff244e457a51b187f980eb965a9
computermouth <computermouth@crunchbangplusplus.org>
2016-03-05 18:44:04 -0800

    Added a few nonsense files, deconstruction shape drawing

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,62 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
 
-struct MyRect {
-	x: i32,
-	y: i32,
-	w: u32,
-	h: u32,
-}
-
-impl MyRect {
-	fn rectin(xin: i32, yin: i32, win: u32, hin: u32) -> MyRect {
-		MyRect {x: xin, y: yin, w: win, h: hin,}
-	}
-	fn rectout(&self) -> Rect {
-		Rect::new(self.x, self.y, self.w, self.h)
-	}
-}
+mod modrect;
+mod shapes;
+//mod game_state;
+//mod action;
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+    let window = video_subsystem.window("SLAPFIGHT", 600, 440)
         .position_centered()
-        //.opengl()
         .build()
         .unwrap();
 
     let mut renderer = window.renderer().build().unwrap();
-    
 	'running: loop {
 		
 		renderer.set_draw_color(Color::RGBA(0, 0, 0, 255));
 		renderer.clear();
-		renderer.set_draw_color(Color::RGBA(125, 0, 125, 255));
 		
-		//let rect = Rect::new(320, 240, 128, 128);
-		//renderer.fill_rect(rect).unwrap();
+		let purp = modrect::MyRect::new()
+			.x(120)
+			.y(120)
+			.w(120)
+			.h(120)
+			.r(200)
+			.g(0)
+			.b(200)
+			.a(120)
+			.finalize();
+			
+
+		renderer = modrect::draw(renderer, purp);
 		
-		let purp = MyRect::rectin(320, 240, 128, 128);
-		renderer.fill_rect(purp.rectout()).unwrap();
+		let green = modrect::MyRect::new()
+			.x(200)
+			.y(200)
+			.w(120)
+			.h(120)
+			.r(0)
+			.g(200)
+			.b(0)
+			.a(120)
+			.finalize();
+			
+		//~ renderer.set_draw_color(green.colorout());
+		//~ renderer.fill_rect(green.rectout()).unwrap();
+
+		renderer = modrect::draw(renderer, green);
+		renderer = modrect::draw(renderer, shapes::RED);
+		
+		//~ let green = modrect::MyRect::new(100, 100, 100, 100, 0, 150, 0, 255);
+		//~ renderer.set_draw_color(green.colorout());
+		//~ renderer.fill_rect(green.rectout()).unwrap();
+		//~ green.draw();
 		
 		renderer.present();
 
